69284a2dca8112d9e80908587c7573bd64416c37
saveroo <saveroo27@gmail.com>
2020-07-23 19:50:29 +0700

    archive(data): old dataset design

---
+++ SRUL/junkgem/DataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace SRUL
+{
+
+
+    partial class DataSet1
+    {
+        partial class VersionsDataTable
+        {
+        }
+
+        partial class DataTable1DataTable
+        {
+        }
+    }
+}
