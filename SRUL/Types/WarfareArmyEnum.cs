f008fa9e0ba8ca2130c8f4453b1c1d690fb1d0f3
saveroo <saveroo27@gmail.com>
2020-07-23 19:05:05 +0700

    feat(main): Warfare Enum

---
+++ SRUL/Types/WarfareArmyEnum.cs
@@ -0,0 +1,38 @@
+﻿namespace SRUL.Types
+{
+    public enum WarfareArmyEnum
+    {
+        Heal,
+        Rambo,
+        GasSupply2x,
+        GasSupply4x,
+        Current,
+        Actual,
+        Fuel,
+        Supply,
+        Experience,
+        Morale,
+        Str2x,
+        Str4x,
+        Str6x,
+        EnemySOON,
+    }
+
+    public enum av
+    {
+        ArmyActiveStaff,
+        ArmyReserve,
+        ArmyActualStrength,
+        ArmyCurrentStrength,
+        ArmySupply,
+        ArmyGas,
+        ArmyEfficiency,
+        ArmyExperience,
+        ArmyMorale,
+        UnitSuppliesCapacity,
+        UnitFuelCapacity,
+        UnitClass,
+        UnitMovementType,
+        UnitTargetType
+    }
+}
