36248201e4ba741dd0a109f51f31ff11f9052977
saveroo <saveroo27@gmail.com>
2020-07-23 19:39:17 +0700

    archive(history): user action history

---
+++ SRUL/ActionHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRUL
+{
+    class ActionHistory
+    {
+        public static List<string> HistoryList = new List<string>();
+
+        public static void addToHistory(string values)
+        {
+            HistoryList.Add(values);
+        }
+    }
+}
