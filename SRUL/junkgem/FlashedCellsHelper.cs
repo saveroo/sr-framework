0d85d4a2985d582a3fdfc669eeb1790d76f726fe
saveroo <saveroo27@gmail.com>
2020-07-23 19:11:36 +0700

    feat(utils): Custom Flashed Grid Cell

---
+++ SRUL/junkgem/FlashedCellsHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.Utils;
+
+namespace SRUL
+{
+    public class FlashedCellsHelper
+    {
+
+        public static AppearanceObject FlashedCellAppearance = new AppearanceObject();
+
+        List<FlashedCell> flashedCells = new List<FlashedCell>();
+        private readonly GridView _View;
+
+        public FlashedCellsHelper(GridView view)
+        {
+            _View = view;
+
+        }
+
+        FlashedCell FindFlashedCell(int rowHanlde, GridColumn col)
+        {
+            foreach (FlashedCell cell in flashedCells)
+            {
+                if (cell.RowHandle == rowHanlde && cell.Column == col)
+                    return cell;
+            }
+
+            FlashedCell result = new FlashedCell(rowHanlde, col, _View);
+            flashedCells.Add(result);
+            return result;
+        }
+
+        public void SetFlashSpeed(int rowHanlde, GridColumn col, int speed)
+        {
+            FindFlashedCell(rowHanlde, col).Speed = speed;
+        }
+    }
+}
