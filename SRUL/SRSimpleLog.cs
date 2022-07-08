d5128a72a36b0ac9264670f8715c15ec67886cfa
Muhammad Surga Savero <saveroo27@gmail.com>
2022-07-08 10:55:41 +0700

    general(): simple log for later?

---
+++ SRUL/SRSimpleLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRUL
+{
+    public struct LogStruct
+    {
+        public string Title;
+        public string Message;
+    }
+
+    public static class SRSimpleLog
+    {
+        public static List<LogStruct> Logs; 
+        public static void RecordLog(string title, string message)
+        {
+            if(Logs == null)
+                Logs = new List<LogStruct>();
+            Logs.Add(new LogStruct()
+            {
+                Title = title,
+                Message = message,
+            });
+            ShowLatestLogs();
+        }
+
+        public static string ShowLatestLogs()
+        {
+            var logs = $"[{Logs.Last().Title}]: {Logs.Last().Message}";
+            Debug.WriteLine(logs);
+            return logs;
+        }
+    }
+}
